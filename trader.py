07267ff083500faa4025e05dfa68d23519d431f9
bera <bera@umich.edu>
2021-05-19 19:06:01 -0400

    FUCK

--- trader.py
+++ trader.py
@@ -1,13 +1,22 @@
+import requests, json
 import alpaca_trade_api as tradeapi
-import requests
 from config import *
 
-url = "https://paper-api.alpaca.markets"
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = f"{BASE_URL}/v2/account"
+ORDERS_URL = f"{BASE_URL}/v2/orders"
+HEADERS = {"APCA-API-KEY-ID":API_KEY, "APCA-API-SECRET-KEY":SECRET_KEY}
 
-r = requests.get(url)
-print(r.content)
+def get_account():
+  r = requests.get(ACCOUNT_URL, headers=HEADERS)
+  return json.loads(r.content)
 
-api = tradeapi.REST(API_KEY, SECRET_KEY, url, api_version='v2')
+def create_order(symbol, qty, side, type, time_in_force):
+  r = requests.post(ORDERS_URL, headers=HEADERS)
+
+  return json.loads(r.content)
+
+api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
 
 account = api.get_account()
 print(account.status)
