e233e7bbb4c70fc70959d081d2f98d0a58290ef0
bera <bera@umich.edu>
2021-05-19 18:00:28 -0400

    Fixed things regarding security

--- trader.py
+++ trader.py
@@ -1,12 +1,13 @@
 import alpaca_trade_api as tradeapi
-import time
-
-key = "PKG1174VEQRPR6K8TTBV"
-sec = "rg8yxCn2LgsrBXUYtR1zhhm9XV0PWeIGEy8DMFMX"
+import requests
+from config import *
 
 url = "https://paper-api.alpaca.markets"
 
-api = tradeapi.REST(key, sec, url, api_version='v2')
+r = requests.get(url)
+print(r.content)
+
+api = tradeapi.REST(API_KEY, SECRET_KEY, url, api_version='v2')
 
 account = api.get_account()
 print(account.status)
