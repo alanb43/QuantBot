548b781180fd76e02cdb10b320ceb1d5b0470ce2
bera <bera@umich.edu>
2021-05-19 23:40:18 -0400

    added link, rearranged trader (no real changes)

--- trader.py
+++ trader.py
@@ -8,11 +8,14 @@ ACCOUNT_URL = f"{BASE_URL}/v2/account"
 ORDERS_URL = f"{BASE_URL}/v2/orders"
 HEADERS = {"APCA-API-KEY-ID":API_KEY, "APCA-API-SECRET-KEY":SECRET_KEY}
 
+api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
+
 def get_account():
   r = requests.get(ACCOUNT_URL, headers=HEADERS)
-  print(r.content)
   return json.loads(r.content)
 
+# Stock Symbol, quantity, buy or sell, market (unless you want to do 
+# limit buy/sell), and day unless you want before / after hours trading
 def create_order(symbol, qty, side, type, time_in_force):
   data = {
     "symbol": symbol,
@@ -25,18 +28,11 @@ def create_order(symbol, qty, side, type, time_in_force):
   r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
   return json.loads(r.content)
 
-
 def get_orders():
   r = requests.get(ORDERS_URL, headers=HEADERS)
   return json.loads(r.content)
 
-api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
-
 account = api.get_account()
-print(account.status)
+my_orders = get_orders()
 
 
-# Stock Symbol, quantity, buy or sell, market (unless you want to do 
-# limit buy/sell), and day unless you want before / after hours trading
-purchase_response = create_order("AAPL", 50, "buy", "market", "day")
-print(purchase_response)
