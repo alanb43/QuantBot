94470eb0076ad10479f71be0c99d55eb3588af86
bera <bera@umich.edu>
2021-05-19 20:59:03 -0400

    trader.py

--- trader.py
+++ trader.py
@@ -1,3 +1,4 @@
+from typing import OrderedDict
 import requests, json
 import alpaca_trade_api as tradeapi
 from config import *
@@ -22,7 +23,11 @@ def create_order(symbol, qty, side, type, time_in_force):
   }
 
   r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+  return json.loads(r.content)
 
+
+def get_orders():
+  r = requests.get(ORDERS_URL, headers=HEADERS)
   return json.loads(r.content)
 
 api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
@@ -31,6 +36,7 @@ account = api.get_account()
 print(account.status)
 
 
-# Stock Symbol, quantity, buy or sell, market (unless you want to do limit buy/sell), and day unless you want before / after hours trading
+# Stock Symbol, quantity, buy or sell, market (unless you want to do 
+# limit buy/sell), and day unless you want before / after hours trading
 purchase_response = create_order("AAPL", 50, "buy", "market", "day")
 print(purchase_response)
