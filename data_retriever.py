fafd9dd5a5aa8e9eb65135e1106dd60ae85ad0a7
bera <bera@umich.edu>
2021-05-23 12:54:06 -0400

    Web Scraping News Articles

--- data_retriever.py
+++ data_retriever.py
@@ -23,41 +23,73 @@ def get_prices(ticker, time_range, interval = '1d'):
   download_path = f'./{ticker}_{time_range}_prices.csv'
   urllib.request.urlretrieve(download_url, download_path)
 
-get_prices('BTC', '1w')
+
+#################################################################
+#                                                               #
+#             FOR WEB SCRAPING NEWS ARTICLES:                   #
+#      this simple script/function retreives all of the         #
+#      financial news stories from marketwatch when used.       #
+#      It stores them in data_retriever_storage/, and also      #
+#      returns this path for the bot's later use.               #
+#                                                               #
+#################################################################
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def scrape_news():
+  # Get the URL and create BeautifulSoup object with its contents
+  url = 'https://www.marketwatch.com/'
+  response = requests.get(url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  storage_path = 'data_retriever_storage/'
+  # Remove the last links present for fresh data (might be overkill)
+  if os.path.exists(storage_path + "mw_a_tags.txt"):
+    os.remove(storage_path + "mw_a_tags.txt")
+  if os.path.exists(storage_path + "mw_unrefined_links.txt"):
+    os.remove(storage_path + "mw_links.txt")
+  
+  # Where we'll store the <a> html elements
+  all_a_tags = open(storage_path + 'mw_a_tags.txt', 'w')
+
+  for a_tag in soup.findAll('a'):
+    all_a_tags.write(str(a_tag))
+
+  all_a_tags.close()
+  all_a_tags = open(storage_path + 'mw_a_tags.txt', 'r')
+  temp_links = open(storage_path + 'mw_unrefined_links.txt', 'w')
+  # Now we read through the <a> elements. A lot of them contains unwanted
+  # nested elements, as well as unuseful info (class, id, style, etc)
+  for line in all_a_tags:
+    # we determine if the if the link is to a story, and add it to temp_links
+    if line.find('href') != -1:
+      link_start = line.find('href') + 6
+      link_end = line.find('>', link_start)
+      link = line[link_start: link_end - 1]
+      if link[:34] == 'https://www.marketwatch.com/story/':
+        temp_links.write(link + '\n')
+
+  temp_links.close()
+  temp_links = open(storage_path + 'mw_unrefined_links.txt', 'r')
+  final_links = open(storage_path + 'mw_links.txt', 'w')
+  # I would've ended here, however the site has a LOT of duplicate links. Now
+  # we'll remove all of the duplicates and finally store the final links
+  lines_seen = set()
+  
+  for line in temp_links:
+    if line not in lines_seen:
+      final_links.write(line)
+      lines_seen.add(line)
+  
+  final_links.close()
+
+  path_to_good_links = storage_path + 'mw_links.txt'
+  return path_to_good_links
+
+
 
 # FOR LATER REFERENCE FOR USING DATETIME
 # import time, datetime
 # period1 = int(time.mktime(datetime.datetime(2018, 5, 22, 23, 59).timetuple()))
 # period2 = int(time.mktime(datetime.datetime(2019, 5, 22, 23, 59).timetuple()))
-
-##########################################################
-#                                                        #
-#   FOR WEB SCRAPE (NOT NECESSARY FOR THIS)              #
-#      moved from scraping because websites hid          #
-#      links too deep for BeautifulSoup to detect        #
-#                                                        #
-##########################################################
-
-#import requests
-#from bs4 import BeautifulSoup
-#from requests.api import get
-#
-# # URL we want to scrape data from
-# url = 'http://web.mta.info/developers/turnstile.html'
-
-# # We connect to the URL (response code 200 desired)
-# response = requests.get(url)
-
-# # We parse the HTML and save it to a BeautifulSoup object
-# soup = BeautifulSoup(response.text, "html.parser")
-
-# # To download the whole data set, let's do a for loop through all a tags
-# line_count = 1 #variable to track what line you are on
-# for one_a_tag in soup.findAll('a'):  #'a' tags are for links
-#     if line_count >= 38: #code for text files starts at line 36
-#         link = one_a_tag['href']
-#         download_url = 'http://web.mta.info/developers/'+ link
-#         urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')+1:]) 
-#         time.sleep(1) #pause the code for a sec
-#     #add 1 for next line
-#     line_count +=1
