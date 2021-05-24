2f596b1752ed3150cda03a6e2b88ac7ef125c9c6
bera <bera@umich.edu>
2021-05-24 19:54:21 -0400

    cleanup, syntax, slight improvements

--- data_retriever.py
+++ data_retriever.py
@@ -1,34 +1,7 @@
 import urllib.request
 import time
 
-# REQUIRES: string representing stock symbol in CAPS (ex : 'TSLA'), string
-#           representing a time range in this format : #w, #m, #y where # is
-#           the number of weeks, months, or years of data you want leading up
-#           to today's data.
-# EFFECTS:  obtains the stock's price history from today to today minus the
-#           entered timerange and stores it in a csv file on the repo.
-# EXAMPLES OF TIME RANGES: '1w' '2y' '6m' 
-def get_prices(ticker, time_range, interval = '1d'): 
-  path = 'data_retriever_storage/prices'
-  if time_range[1] == 'w':
-    period = int(time_range[0]) * int(31536000 / 52)
-  elif time_range[1] == 'm':
-    period = int(time_range[0]) * int(31536000 / 12)
-  elif time_range[1] == 'y':
-    period = int(time_range[0]) * 31536000
-  else:
-    return "ERROR: Invalid time_range entered. See examples"
-  period2 = int(time.time())
-  period1 = period2 - period
-  download_url = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
-  download_path = f'./{path}/{ticker}_prices.csv'
-  urllib.request.urlretrieve(download_url, download_path)
-
-#get_prices('AAPL', '1y')
-
-import requests
-from bs4 import BeautifulSoup
-import os
+############################### HELPER FUNCTIONS #########################
 
 # REQUIRES: list of filenames to remove, the directory where they're stored
 # EFFECTS: removes the files from the directory if they exist
@@ -62,7 +35,48 @@ def remove_duplicate_lines(input, output, path):
   file_out.close()
 
 
+##########################################################################
+#               SIMPLE API REQUEST FOR PRICE HISTORY                     #
+##########################################################################
+
+# REQUIRES: string representing stock symbol in CAPS (ex : 'TSLA'), string
+#           representing a time range in this format : #w, #m, #y where # is
+#           the number of weeks, months, or years of data you want leading up
+#           to today's data.
+# EFFECTS:  obtains the stock's price history from today to today minus the
+#           entered timerange and stores it in a csv file on the repo.
+# EXAMPLES OF TIME RANGES: '1w' '2y' '6m' 
+def get_prices(ticker, time_range, interval = '1d'): 
+  path = 'data_retriever_storage/prices'
+  if time_range[1] == 'w':
+    period = int(time_range[0]) * int(31536000 / 52)
+  elif time_range[1] == 'm':
+    period = int(time_range[0]) * int(31536000 / 12)
+  elif time_range[1] == 'y':
+    period = int(time_range[0]) * 31536000
+  else:
+    return "ERROR: Invalid time_range entered. See examples"
+  period2 = int(time.time())
+  period1 = period2 - period
+  download_url = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+  download_path = f'./{path}/{ticker}_prices.csv'
+  urllib.request.urlretrieve(download_url, download_path)
+
+#get_prices('AAPL', '1y')
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+##########################################################################
+#      WEB SCRAPING USING BEAUTIFUL SOUP AND BUILT-IN HTML PARSER        #
+##########################################################################
+
+# NOTE : FUNCTIONS USED THAT AREN'T DEFINED YET ARE AT THE BOTTOM OF FILE
+
 # REQUIRES: a string representing a stock ticker
+# MODIFIES: technically modifies files that we use to grab desired links
+#           to our output file, but this file is deleted (end of func)
 # EFFECTS: Accesses MarketWatch, searches the ticker's page for news articles,
 #          finds, filters, and individualizes links to articles. Creates a file
 #          containing links to all of the ticker's stories, returns its path.
@@ -99,9 +113,12 @@ def scrape_news_links(ticker):
   
   return path + files[2]
 
-scrape_news_links('tsla')
-
-
+# REQUIRES: string of article link, int number associated with output
+#           file name, string ticker representing associated stock
+# MODIFIES: technically modifies a file that we use to move desired text
+#           to our output file, but this file is deleted (end of func)
+# EFFECTS:  Parses the entire article webpage's text for article info
+#           that we want, storing it in a file at the path.
 def scrape_news_data(url, article_num, ticker):
   soup = create_soup(url)
   path = './data_retriever_storage/news/news_article_contents/'
@@ -126,12 +143,37 @@ def scrape_news_data(url, article_num, ticker):
   file1.close()
   remove_files([input], path)
 
+#######################################################################
+## THIS FUNCTION IS THE ONE THE BOT WILL USE, ABSTRACTS DETAILS AWAY ##
+#######################################################################
 
-scrape_news_data('https://www.marketwatch.com/articles/ford-stock-electric-f150-lightning-launch-tesla-51621516206?mod=mw_quote_news', 1, 'tsla')
-scrape_news_data('https://www.marketwatch.com/articles/ford-ev-f-150-lightning-orders-rush-in-the-stock-is-rising-51621606918?mod=mw_quote_news', 2, 'tsla')
-scrape_news_data('https://www.marketwatch.com/articles/tesla-stock--price-outlook-hold-rating-51621866627?mod=mw_quote_news', 3, 'tsla')
+# REQUIRES: a string representing a valid ticker / symbol
+# MODIFIES: news folder's subfolders (populates them)
+# EFFECTS: gathers news links related to a ticker, parses through
+#          each link for article content, stores individual contents
+#          in numbered files.
+# NOTE: at some point the deletion of past stock news should be added
+#       before the new article contents are stored, as well as the 
+#       updating of paths to reflect individual tickers for sorting and 
+#       storage simplicity.
+def get_stock_news(ticker):
+  scrape_news_links(ticker)
+  path = './data_retriever_storage/news/news_links/'
+  if os.path.exists(path + f'mw_{ticker}_links.txt'):
+    links_file = open(path + f'mw_{ticker}_links.txt')
+  else:
+    return "Links haven't been scraped. Use scrape_news_links"
+  link_number = 1
+  for link in links_file:
+    scrape_news_data(link, link_number, ticker)
+    link_number += 1
 
-# FOR LATER REFERENCE FOR USING DATETIME
+get_stock_news('tsla')
+
+
+
+
+# FOR LATER REFERENCE IF USING DATETIME
 # import time, datetime
 # period1 = int(time.mktime(datetime.datetime(2018, 5, 22, 23, 59).timetuple()))
 # period2 = int(time.mktime(datetime.datetime(2019, 5, 22, 23, 59).timetuple()))
