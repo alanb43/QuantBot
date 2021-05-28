b5672bfe17f09a4f9803203ab2188c53a60f280f
bera <bera@umich.edu>
2021-05-27 22:56:48 -0400

    sentiment_analyzer created

---
+++ sentiment_analyzer.py
@@ -0,0 +1,10 @@
+from nltk.corpus import twitter_samples
+
+
+
+positive_tweets = twitter_samples.strings('positive_tweets.json')
+negative_tweets = twitter_samples.strings('negative_tweets.json')
+text = twitter_samples.strings('tweets.20150430-223406.json')
+tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+more_tokens = twitter_samples.tokenized('negative_tweets.json')
+
