229b2cbd404b29a3d982ccb7f08d3cd245b252b0
bera <bera@umich.edu>
2021-05-28 16:19:24 -0400

    Tokenizing

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,21 +1,42 @@
 from nltk.corpus import twitter_samples
 
-positive_tweets = twitter_samples.strings('positive_tweets.json')
-negative_tweets = twitter_samples.strings('negative_tweets.json')
-text = twitter_samples.strings('tweets.20150430-223406.json')
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-more_tokens = twitter_samples.tokenized('negative_tweets.json')
+# positive_tweets = twitter_samples.strings('positive_tweets.json')
+# negative_tweets = twitter_samples.strings('negative_tweets.json')
+# text = twitter_samples.strings('tweets.20150430-223406.json')
+# tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+# more_tokens = twitter_samples.tokenized('negative_tweets.json')
 
-path = 'data_retriever_storage/news/news_article_contents/AAPL/AAPL17.txt'
+path = 'data_retriever_storage/news/news_article_contents/AAPL/AAPL4.txt'
 
 file = open(path, 'r')
 
-outfile = open('output.txt', 'w')
 
-for line in file:
-  outfile.write(line)
+# keep_skipping = True
+# article_words = []
+
+# for line in file:
+#   if len(line.split()) > 2:
+#     keep_skipping = False
+#   if keep_skipping or len(line.split()) == 0:
+#     continue
+#   a = list(line.split())
+#   article_words += a
+
+# for word in article_words:
+#   print(word)
+
 
 class SentimentAnalyzer:
+  '''
+  Class should support
+    tokenizing
+    removing noise
+    determining density
+    the actual model
+    testing the model
+  
+  
+  '''
   def __init__(self) -> None:
-      pass
+    pass
 
