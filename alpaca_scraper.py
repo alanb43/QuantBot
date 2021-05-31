36132eb1243aea3b3f9adda796d935cb1b9cd1bf
jcun <jcun@umich.edu>
2021-05-31 15:13:47 -0400

    idk what this is

--- alpaca_scraper.py
+++ alpaca_scraper.py
@@ -3,16 +3,16 @@ from trader import *
 T = Trader()
 data = T.get_account()
 
-EQUITY = data[]
-DAILY_CHANGE = data[]
-BUYING_POWER = data[]
-PERCHANGE = data[]
+# EQUITY = data[]
+# DAILY_CHANGE = data[]
+# BUYING_POWER = data[]
+# PERCHANGE = data[]
 
-AAPL_VALUE = data[]
-AAPL_PERCHANGE = data[]
+# AAPL_VALUE = data[]
+# AAPL_PERCHANGE = data[]
 
-TSLA_VALUE = data[]
-TSLA_PERCHANGE = data[]
+# TSLA_VALUE = data[]
+# TSLA_PERCHANGE = data[]
 
 
 
