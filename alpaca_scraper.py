048d32971b51b4917cfe64f3ad0c3d8e24e246a4
Josh Cunningham <>
2021-05-31 04:06:45 +0000

    prepping to pull alpaca data

---
+++ alpaca_scraper.py
@@ -0,0 +1,19 @@
+from trader import *
+
+T = Trader()
+data = T.get_account()
+
+EQUITY = data[]
+DAILY_CHANGE = data[]
+BUYING_POWER = data[]
+PERCHANGE = data[]
+
+AAPL_VALUE = data[]
+AAPL_PERCHANGE = data[]
+
+TSLA_VALUE = data[]
+TSLA_PERCHANGE = data[]
+
+
+
+print(data)
--- webpage/alpaca_scraper.py
+++
@@ -1,4 +0,0 @@
-# webscrape alpaca, store values needed for website in a python file,
-# link the python to the html
-
-# 
