7b57aee56c63e90c420d734fbf585844fcac629d
bera <bera@umich.edu>
2021-06-08 01:09:37 -0400

    GCP SWAG

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+  return "Fuck you Josh"
+
+if __name__ == "__main__":
+  app.run(host="127.0.0.1", port=8080, debug=True)
