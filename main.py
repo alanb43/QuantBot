c769fed82bed43d99a1d5fe007539b27d3c5e0c9
bera <bera@umich.edu>
2021-06-08 01:32:38 -0400

    A whole lotta organizing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-  return render_template("webpage/index.html") 
+  return render_template("index.html") 
 
 if __name__ == "__main__":
   app.run(host="127.0.0.1", port=8080, debug=True)
