aca99c001057cfb571512a1494488138aa9337fe
bera <bera@umich.edu>
2021-06-30 01:07:52 -0400

    Updated delete_db

--- delete_db.py
+++ delete_db.py
@@ -2,10 +2,10 @@ import sqlite3
 connection = sqlite3.connect('databases/stock-data.db')
     
 cursor = connection.cursor()
-cursor.execute("""
-    DROP TABLE stock_price
-""")
-cursor.execute("""
-    DROP TABLE stock
-""")
+cursor.execute("""DROP TABLE stock""")
+cursor.execute("""DROP TABLE stock_price""")
+cursor.execute("""DROP TABLE stock_news""")
+cursor.execute("""DROP TABLE sentiment_data""")
+cursor.execute("""DROP TABLE time_series_data""")
+
 connection.commit()
