463a81d96b5eb0f09cd466cd54ff3c5bff6e8570
bera <bera@umich.edu>
2021-06-29 21:36:31 -0400

    Updated create to have all schemas in current design plan

--- create_db.py
+++ create_db.py
@@ -22,5 +22,43 @@ cursor.execute("""
         FOREIGN KEY (stock_id) REFERENCES stock (id)
     )
 """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS sentiment_data (
+        word PRIMARY KEY, 
+        category NOT NULL,
+        sentiment NOT NULL,
+        frequency INTEGER NOT NULL,
+        stock_id INTEGER,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS time_series_data (
+        id INTEGER PRIMARY KEY, 
+        stock_id INTEGER,
+        symbol TEXT,
+        mape FLOAT NOT NULL,
+        one_week_fc FLOAT NOT NULL,
+        one_month_fc FLOAT NOT NULL,
+        one_week_actual FLOAT NOT NULL,
+        one_month_actual FLOAT NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock_news (
+        id INTEGER PRIMARY KEY, 
+        stock_id INTEGER,
+        date_retrieved NOT NULL,
+        url NOT NULL,
+        analyzed BIT NOT NULL,
+        article_content NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+    )
+""")
+
 connection.commit()
 
